c8d6458077f7edb2f0bdcefbf4e95d71746ba126
Laura Caroselli <lcaroselli@outlook.com>
2017-08-22 18:36:24 -0600

    Add fetch vehicles

--- src/App.js
+++ src/App.js
@@ -9,14 +9,16 @@ export default class App extends Component {
   constructor() {
     super()
     this.state = {
-      people:   null,
-      openingText: [],
+      people: null,
+      vehicles: null,
+      openingText: []
     }
   }
 
   componentDidMount(films) {
     this.fetchMovieOpening()
     this.fetchPeopleData()
+    this.fetchVehicleData()
   }
 
   fetchMovieOpening() {
@@ -37,8 +39,8 @@ export default class App extends Component {
     })
   }
 
-    fetchPeopleData() {
-      fetch(`https://swapi.co/api/people/`)
+  fetchPeopleData() {
+    fetch(`https://swapi.co/api/people/`)
       .then(data => data.json())
       .then(data => this.fetchSpecificPeopleData(data.results))
       .then(newPeople =>
@@ -46,20 +48,36 @@ export default class App extends Component {
           people: newPeople
         })
       )
-    }
+  }
 
-    fetchSpecificPeopleData(data) {
-      const unresolvedPromises = data.map(person => {
-        return fetch(person.homeworld)
-        .then(response => response.json())
-
-      })
-
-      return Promise.all(unresolvedPromises)
+  fetchSpecificPeopleData(data) {
+    const unresolvedPromises = data.map(person => {
+      return fetch(person.homeworld)
+      .then(response => response.json())
+    })
+    return Promise.all(unresolvedPromises)
       .then(planet => { return planet.map((world, index, array) => {
         return Object.assign({planet: world.name, population: world.population}, data[index])
       })
     })
+  }
+
+  fetchVehicleData() {
+    fetch(`https://swapi.co/api/vehicles/`)
+      .then(data => data.json())
+      .then(data => (data.results).map((vehicle)=>{
+        return {
+          vehicleName: vehicle.name,
+          model: vehicle.model,
+          vehicleClass: vehicle.vehicle_class,
+          passengers: vehicle.passengers
+        }
+        }))
+      .then(data => {
+        this.setState({
+          vehicles: data
+        })
+      })
   }
 
   render() {
